9c2313ac49087147269973d4510016270172dec7
lokesh108 <46420483+lokesh108@users.noreply.github.com>
2020-10-25 22:53:25 +0530

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -4,7 +4,7 @@
 import datetime
 
 name = input('Please provide your name. ')
-age = int(input('What is your age?'))
-turn100 = datetime.datetime.now().year + (100 - age)
-printTimes = int(input('Please provide a number. '))
-print(('Hello ' + name + '.  You will turn 100 years old in ' + str(turn100) + '\n') * printTimes)
+age = int(input('how old are you'))
+current_year=input('Enter current year)
+year=100-age
+print((f'Hello {name} . You will turn 100 years old in {year}')
